aa0ca23d034ccaea03d4661fcae136ebf36f1180
bike_heero_in_Nepal <2202004@ritindia.edu>
2025-03-23 16:11:21 +0530

    i add a project

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp())
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoScreen(),
+    );
+  }
+}
+
+class TodoScreen extends StatefulWidget {
+  const TodoScreen({super.key});
+
+  @override
+  _TodoScreenState createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
+  List<Map<String, dynamic>> _tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTasks();
+  }
+
+  _loadTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? tasksString = prefs.getString('tasks');
+    if (tasksString != null) {
+      setState(() {
+        _tasks = List<Map<String, dynamic>>.from(json.decode(tasksString));
+      });
+    }
+  }
+
+  _saveTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('tasks', json.encode(_tasks));
+  }
+
+  _addTask(String task) {
+    setState(() {
+      _tasks.add({'title': task, 'completed': false});
+      _saveTasks();
+    });
+  }
+
+  _toggleTask(int index) {
+    setState(() {
+      _tasks[index]['completed'] = !_tasks[index]['completed'];
+      _saveTasks();
+    });
+  }
+
+  _deleteTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+      _saveTasks();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController taskController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: Text("To-Do List")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: taskController,
+                    decoration: InputDecoration(labelText: "New Task"),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    if (taskController.text.isNotEmpty) {
+                      _addTask(taskController.text);
+                      taskController.clear();
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    _tasks[index]['title'],
+                    style: TextStyle(
+                      decoration: _tasks[index]['completed']
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                  leading: Checkbox(
+                    value: _tasks[index]['completed'],
+                    onChanged: (value) => _toggleTask(index),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _deleteTask(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
